1f0f57e7c58d5cbaddc1a6f00d62cf821e10f6fb
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-03-02 00:04:47 -0300

    iniciando desafio

---
+++ react-contador/src/App.js
@@ -0,0 +1,19 @@
+//desafio contaodr nao iniciado
+
+import React, {Component} from 'react';
+// import styleCss from "./files/style.css" -- nao sei se precisa
+
+import styled from "style-component";
+const main = styled.main`
+background-color: pink`
+class Contador extends Component{
+
+
+  render(){
+    return(
+   <main>
+
+   </main>
+    )
+  }
+}export default Contador
