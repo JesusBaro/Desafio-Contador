5c24011fdae7e4728eefc8a1a6c38b3a102dbde4
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-03-03 23:42:51 -0300

    últimas atualizações

--- react-contador/src/App.js
+++ react-contador/src/App.js
@@ -1,14 +1,13 @@
+//As partes comentadas são experimentos, porém as partes do desafio está completo.
+//S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2
+
+
+
+
 import React, { Component } from 'react';
-
-
 import styled from "styled-components";
 import backgroundMain from "./files/back5.png";
 
-const Img = styled.img`
-width: 40vw;
-z-indez: 0;
-position: fixed;
-`
 const Container = styled.section`
 width: 100vw;
 height: 100vh; 
@@ -23,12 +22,16 @@ height: 80vh;
 display: flex;
 align-items: center;
 justify-content: center;
+background-image:url(${backgroundMain});
+background-size:contain;
+background-position: center;
+background-repeat: no-repeat;
 `
 const H2 = styled.h2`
 font-size: 3.5em;
 color: white;
-text-align: center;
-`
+text-align: center;`
+
 const BoxView = styled.div`
 width: 18vw;
 height: 32vh;
@@ -50,73 +53,102 @@ justify-content: center;
 
 const BoxButtom = styled.div`
 width: 18vw;
-height: 10vh;
+height: 20vh;
 display: flex;
 align-items: center;
 justify-content: space-evenly;
+flex-wrap:wrap;
 `
 const ButtonView = styled.button`
-  width: 3.5vw;
-  height: 5vh;
-  font-size: 1.4rem;
+
+  width: 7vw;
+  height: 5.5vh;
+  font-size: 1rem;
   text-align: center;
   border-radius: 10vh;
   background-color: black;
-  border: solid white;
+  border:transparent;
   color: white;
   transition: all 0.5s ease-in-out;
 
-  &:hover {
-    border-color: tomato;
+  &:focus {
     color: tomato;
-    opacity: .8;
+    opacity: 0.9;
   }`
 
 class Contador extends Component {
-  
+
   state = {
-    numero: 0,
-    Arquivos: [
-      {
-        background: `${backgroundMain}`,
-        simboloMais: " + ",
-        simboloMenos: " - ",
-      }
-    ]
+    numero: 0
   }
 
- 
+
   adicionar = () => {
-    if (this.state.numero < 10) {
+    const reset = setInterval(() => {
+      if (this.state.numero < 10) {
+        this.setState({
+          numero: this.state.numero + 1
+        });
+      }
+
+    }, 1000);
+    this.reset = () => {
+      clearInterval(reset)
       this.setState({
-        numero: this.state.numero + 1
+        numero: this.state.numero
       });
     }
+
   };
 
-  remover = () => {
-    if (this.state.numero > 0) {
-      this.setState({
-        numero: this.state.numero - 1
-      });
-    }
-  };
+  return = () => {
+    setInterval(() => {
+      if (this.state.numero > 0) {
+        this.setState({
+          numero: this.state.numero - 1
+        });
+      }
+    }, 1000);    
+  }
 
- 
+
+  //------
+  //TENTANDO FAZER COM QUE QUANDO A FUNÇÃO CHEGAR A ZERO ELA PARE SÓZINHA
+
+  // return = () => {
+  //   setInterval(() => {
+  //     if (this.state.numero > 0) {
+  //       this.setState({
+  //         numero: this.state.numero - 1
+  //       });
+  //       if (this.state.numero = 0) {
+  //         this.setState({
+  //           numero: "finalizado"
+  //         });
+  //       }
+  //     }
+  //   }, 1000);
+  // }
+
+
+
+
   render() {
     return (
       <Container>
 
         <BoxBackground>
-          <Img className="backgroundMain" src={this.state.Arquivos[0].background} alt="" />
           <BoxView>
             <BoxNumber>
               <H2>{this.state.numero}</H2>
             </BoxNumber>
 
             <BoxButtom>
-              <ButtonView onClick={this.adicionar}>+</ButtonView>
-              <ButtonView onClick={this.remover}>-</ButtonView>
+              <ButtonView onClick={this.adicionar}>Go</ButtonView>
+              <ButtonView onClick={this.return}>Return</ButtonView>
+              <ButtonView onClick={this.reset}>Stop</ButtonView>
+              {/* <ButtonView >Clear</ButtonView>*/}
+
             </BoxButtom>
           </BoxView>
 
